6319c75e0d4a53923dec670ca79979e9e5be0a08
Anhilyx <anhilyx@gmail.com>
2025-10-22 11:41:41 +0200

    Models are now starting (but tokenization fails)

--- Skynet_Monitor/app/src/main/java/resc/ai/skynetmonitor/service/NativeLlama.kt
+++ Skynet_Monitor/app/src/main/java/resc/ai/skynetmonitor/service/NativeLlama.kt
@@ -4,6 +4,9 @@ object NativeLlama {
     init {
         System.loadLibrary("llama")
     }
+    init {
+        System.loadLibrary("native-lib") // doit être appelé avant d'utiliser initModel
+    }
 
     external fun initModel(modelPath: String): Boolean
     external fun generate(prompt: String): String?
